1c7d005684f6899fe8a55cff69fa62ba54db1dea
quantra <vincentjoncoleman@gmail.com>
2023-05-21 18:59:05 +0100

    Reorganised some tests

--- content_blocks/tests/test_views.py
+++ content_blocks/tests/test_views.py
@@ -223,3 +223,14 @@ class TestDeleteContentBlock:
 
         assert response.status_code == 200
         assert ContentBlock.objects.count() == 0
+
+
+class TestPermissions:
+    @pytest.mark.django_db
+    def test_ajax_required(self, admin_client, content_block):
+        response = admin_client.post(
+            reverse("content_blocks:content_block_delete", args=[content_block.id]),
+            {},
+        )
+
+        assert response.status_code == 403
