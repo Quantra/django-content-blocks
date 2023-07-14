3362f40d0ad95784cb4ed5774dbedca81d6ab2e4
quantra <vincentjoncoleman@gmail.com>
2023-07-14 17:27:55 +0100

    deleted test_cache_setting.py

--- example/settings/test_cache_setting.py
+++
@@ -1,8 +0,0 @@
-from .base import *  # noqa
-
-CACHES = CACHES.copy()  # noqa
-CACHES["content_blocks"] = {
-    "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-    "LOCATION": "content_blocks",
-}
-CONTENT_BLOCKS_CACHE = "content_blocks"
