2737a0c270ff117f55488169db2a1eb072df02f3
quantra <vincentjoncoleman@gmail.com>
2023-09-05 22:13:18 +0100

    Chore: Updated prettier and ran it

--- content_blocks/static/content_blocks/js/popup.js
+++ content_blocks/static/content_blocks/js/popup.js
@@ -3,9 +3,9 @@ let open_popups = { popups: [], callers: [] };
 let $popup_base;
 
 $(document).ready(function () {
-  $html = $('html');
-  $body = $('body');
-  $popup_base = $('#popup_base');
+  $html = $("html");
+  $body = $("body");
+  $popup_base = $("#popup_base");
 });
 
 function showPopup(caller, kwargs) {
@@ -22,29 +22,29 @@ function showPopup(caller, kwargs) {
   disableScroll();
 
   let $popup = $popup_base.clone();
-  $popup.attr('id', '');
+  $popup.attr("id", "");
   open_popups.popups.push($popup);
   open_popups.callers.push(caller);
 
   $popup
-    .on('click', function () {
+    .on("click", function () {
       hidePopup($popup);
     })
-    .on('click', '.confirm', function () {
+    .on("click", ".confirm", function () {
       hidePopup($popup);
       if (kwargs.confirm_callback) kwargs.confirm_callback($popup);
     })
-    .on('click', '.close-popup', function () {
+    .on("click", ".close-popup", function () {
       hidePopup($popup);
     })
-    .on('click', '.popup-wrapper', function (e) {
+    .on("click", ".popup-wrapper", function (e) {
       e.stopPropagation();
     });
 
   if (kwargs.css_class) $popup.addClass(kwargs.css_class);
 
   if (kwargs.content) {
-    $popup.find('.popup-content').html(kwargs.content);
+    $popup.find(".popup-content").html(kwargs.content);
   }
 
   $body.append($popup);
@@ -81,12 +81,12 @@ function openPopupsRemove(open_popups, popup) {
 function disableScroll() {
   if ($(document).height() > $(window).height()) {
     let scrollTop = $html.scrollTop() ? $html.scrollTop() : $body.scrollTop(); // Works for Chrome, Firefox, IE...
-    $html.addClass('noscroll').css('top', -scrollTop);
+    $html.addClass("noscroll").css("top", -scrollTop);
   }
 }
 
 function enableScroll() {
-  let scrollTop = parseInt($html.css('top'));
-  $html.removeClass('noscroll');
-  $('html,body').scrollTop(-scrollTop);
+  let scrollTop = parseInt($html.css("top"));
+  $html.removeClass("noscroll");
+  $("html,body").scrollTop(-scrollTop);
 }
